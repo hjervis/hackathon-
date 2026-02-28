fbe10769db682255ae5881f59ef054ebd85414ff
gabeninjaboy <gabriel.j.holfester@gmail.com>
2026-02-28 01:42:59 -0500

    Added session controller and service

---
+++ backend/Controllers/location_sessionController.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from Database.database import get_db, engine
+import Models
+from Services import location_session_service
+
+app = FastAPI()
+
+@app.post("/users/{user_id}/sessions")
+def create_session(user_id: int, db: Session = Depends(get_db)):
+    return location_session_service.createLocationSession(db, user_id)
+
+@app.put("/users/{user_id}/sessions/{session_id}/end")
+def end_session(user_id: int, session_id: int, db: Session = Depends(get_db)):
+    return location_session_service.end_session(db, user_id, session_id)
+
+@app.get("/users/{user_id}/sessions/active")
+def get_active_session(user_id: int, db: Session = Depends(get_db)):
+    return location_session_service.get_active_session(db, user_id)
+
+@app.get("/users/{user_id}/sessions")
+def get_all_sessions(user_id: int, db: Session = Depends(get_db)):
+    return location_session_service.get_all_sessions(db, user_id)
